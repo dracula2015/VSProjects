ec6dd2eef96cc8f2f2ee6ea17e2dd7318ab76d1e
dracula <draculamars@hotmail.com>
2017-12-17 07:10:54 +0800

    basic computational functions debugging finished

--- dsPIC_oriented_C_develop_ANSI_C/dsPIC_oriented_C_develop_ANSI_C/dsPIC_oriented_C_develop.c
+++ dsPIC_oriented_C_develop_ANSI_C/dsPIC_oriented_C_develop_ANSI_C/dsPIC_oriented_C_develop.c
@@ -1,7 +1,4 @@
 // dsPIC_oriented_C_develop.cpp : Defines the entry point for the console application.
-//
-
-//#include "stdafx.h"
 
 #include "user.h"
 int counter = 0;
@@ -9,7 +6,7 @@ int countMatrix = 0;
 int countVector = 0;
 float triMatrix[3][3] = { 6,0,0,0,6,0,0,0,6 };
 Matrix* pointerMatrix[100] = { NULL };
-Vector3f* pointerVector[] = { NULL };
+Vector3f* pointerVector[100] = { NULL };
 Parameter P;
 Matrix *Kp = NULL;
 Matrix *Kd = NULL;
@@ -23,97 +20,16 @@ Matrix *Kd;
 
 int main()
 {
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n\n", Kd);
+
 	Kp = m_constructor(NULL, NULL, 6, 0, 0, 0, 6, 0, 0, 0, 6);
-	printf("address of Kp: %p\n\n", Kp);
 	Kd = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n\n", Kd);
-	Matrix * Kd1 = m_constructor(NULL, NULL, 10.1, 0.2, 0.3, 0, 10, 0, 0, 0, 10);
-	
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n", Kd);
-	printf("address of Kd1: %p\n\n", Kd1);
-	printf("pointerMatrix 0: %p\n", pointerMatrix[0]);
-	printf("pointerMatrix 1: %p\n", pointerMatrix[1]);
-	printf("pointerMatrix 2: %p\n", pointerMatrix[2]);
-	Matrix * Kd2 = m_constructor(NULL, NULL, 100.1, 101.2, 102.3, 0, 10, 0, 0, 0, 10);
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n", Kd);
-	printf("address of Kd1: %p\n", Kd1);
-	printf("address of Kd2: %p\n\n", Kd2);
-	Matrix * Kd3 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n", Kd);
-	printf("address of Kd1: %p\n", Kd1);
-	printf("address of Kd2: %p\n", Kd2);
-	printf("address of Kd3: %p\n\n", Kd3);
-	Matrix * Kd4 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n", Kd);
-	printf("address of Kd1: %p\n", Kd1);
-	printf("address of Kd2: %p\n", Kd2);
-	printf("address of Kd3: %p\n", Kd3);
-	printf("address of Kd4: %p\n\n", Kd4);
-	Matrix * Kd5 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n", Kd);
-	printf("address of Kd1: %p\n", Kd1);
-	printf("address of Kd2: %p\n", Kd2);
-	printf("address of Kd3: %p\n", Kd3);
-	printf("address of Kd4: %p\n", Kd4);
-	printf("address of Kd5: %p\n\n", Kd5);
-	Matrix * Kd6 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	printf("address of Kp: %p\n", Kp);
-	printf("address of Kd: %p\n", Kd);
-	printf("address of Kd1: %p\n", Kd1);
-	printf("address of Kd2: %p\n", Kd2);
-	printf("address of Kd3: %p\n", Kd3);
-	printf("address of Kd4: %p\n", Kd4);
-	printf("address of Kd5: %p\n", Kd5);
-	printf("address of Kd6: %p\n\n", Kd6);
-	//Matrix * Kd1 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd2 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd3 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd4 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd5 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd6 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd7 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd8 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd9 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd10 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd11 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	//Matrix * Kd12 = m_constructor(NULL, NULL, 10, 0, 0, 0, 10, 0, 0, 0, 10);
-	/*Matrix * Kd1 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd2 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd3 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd4 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd5 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd6 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd7 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd8 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd9 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd10 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd11 = (Matrix*)malloc(sizeof(Matrix));
-	Matrix * Kd12 = (Matrix*)malloc(sizeof(Matrix));*/
-	/*Vector3f* qd = v_constructor(NULL, 1, 2, 3);
+
+	Vector3f* qd = v_constructor(NULL, 1, 2, 3);
 	Vector3f* dqd = v_constructor(NULL, 4, 5, 6);
 	Vector3f* ddqd = v_constructor(NULL, 2, 3, 4);
 	Vector3f* q = v_constructor(NULL, 5, 6, 7);
-	Vector3f* dq = v_constructor(NULL, 1, 5, 9);*/
-	
-	Matrix *dd = (Matrix*)malloc(sizeof(Matrix));
-	Matrix tt;
-	
-	tt.triMatrix[0][0] = 11;
-	printf("tt: %p", tt);
-	tt.triMatrix[0][1] = 12;
-	tt.triMatrix[0][2] = 13;
-	
-	dd->triMatrix[0][0] = 21;
-	dd->triMatrix[0][1] = 22;
-	dd->triMatrix[0][2] = 23;
+	Vector3f* dq = v_constructor(NULL, 1, 5, 9);
+
 	P.m = 11.4;
 	P.Iv = 0.65;
 	P.r = 0.05;
@@ -132,55 +48,58 @@ int main()
 	P.beta1 = pow(P.n, 2) * (P.b0 + P.kt*P.kb / P.Ra) / pow(P.r, 2);
 	P.beta2 = P.n*P.kt / P.r / P.Ra;
 
-	//OMRS_controller(qd, dqd, ddqd, q, dq);
-	int a, b, c;
-	char d;
-	//a = 1;
-	b = 2;
-	c = 3;
-	float w = 100.123123123;
-	double dl = 100.123123123;
-	long double longdl = 100.123123123;
+	Vector3f *controlEffect = OMRS_controller(qd, dqd, ddqd, q, dq);
+	int a;
 	while (1) {
 		scanf_s("%d", &a);
-		//printf("%d,%d", &tt->triMatrix[1][2], &tt->triMatrix[2][0]);
-		printf("%d,%d\n", a, c);
-		printf("tt: %f,%f,%f\n", tt.triMatrix[0][0], tt.triMatrix[0][1], tt.triMatrix[0][2]);
-		//printf("%f,%f,%f\n", triMatrix[0][0], triMatrix[0][1], triMatrix[0][2]);
-		printf("dd: %f,%f,%f\n", dd->triMatrix[0][0], dd->triMatrix[0][1], dd->triMatrix[0][2]);
 
-		//printf("Kd1\n");
-		//printf("%f,%f,%f\n", Kd1->triMatrix[0][0], Kd1->triMatrix[0][1], Kd1->triMatrix[0][2]);
-		//printf("Kd2\n");
-		//printf("%f,%f,%f\n", Kd2->triMatrix[0][0], Kd2->triMatrix[0][1], Kd2->triMatrix[0][2]);
+		printf("Kp\n");
+		printf("%f,%f,%f\n", Kp->triMatrix[0][0], Kp->triMatrix[0][1], Kp->triMatrix[0][2]);
+		printf("%f,%f,%f\n", Kp->triMatrix[1][0], Kp->triMatrix[1][1], Kp->triMatrix[1][2]);
+		printf("%f,%f,%f\n", Kp->triMatrix[2][0], Kp->triMatrix[2][1], Kp->triMatrix[2][2]);
+		printf("Kd\n");
+		printf("%f,%f,%f\n", Kd->triMatrix[0][0], Kd->triMatrix[0][1], Kd->triMatrix[0][2]);
+		printf("%f,%f,%f\n", Kd->triMatrix[1][0], Kd->triMatrix[1][1], Kd->triMatrix[1][2]);
+		printf("%f,%f,%f\n", Kd->triMatrix[2][0], Kd->triMatrix[2][1], Kd->triMatrix[2][2]);
+		printf("qd\n");
+		printf("%f,%f,%f\n", qd->x, qd->y, qd->z);
+		printf("dqd\n");
+		printf("%f,%f,%f\n", dqd->x, dqd->y, dqd->z);
+		printf("ddqd\n");
+		printf("%f,%f,%f\n", ddqd->x, ddqd->y, ddqd->z);
+		printf("q\n");
+		printf("%f,%f,%f\n", q->x, q->y, q->z);
+		printf("dq\n");
+		printf("%f,%f,%f\n", dq->x, dq->y, dq->z);
+		printf("P.m: %f\n", P.m);
+		printf("P.Iv: %f\n", P.Iv);
+		printf("P.r: %f\n", P.r);
+		printf("P.Din: %f\n", P.Din);
+		printf("P.Dout: %f\n", P.Dout);
+		printf("P.La: %f\n", P.La);
+		printf("P.I0: %f\n", P.I0);
+		printf("P.kt: %f\n", P.kt);
+		printf("P.kb: %f\n", P.kb);
+		printf("P.n: %f\n", P.n);
+		printf("P.b0: %f\n", P.b0);
+		printf("P.Ra: %f\n", P.Ra);
+		printf("P.beta0: %f\n", P.beta0);
+		printf("P.beta1: %f\n", P.beta1);
+		printf("P.beta2: %f\n", P.beta2);
 
-		//printf("Kp\n");
-		//printf("%f,%f,%f\n", Kp->triMatrix[0][0], Kp->triMatrix[0][1], Kp->triMatrix[0][2]);
-		//printf("Kd\n");
-		//printf("%f,%f,%f\n", Kd->triMatrix[0][0], Kd->triMatrix[0][1], Kd->triMatrix[0][2]);
-		printf("countMatrix: %p\n", countMatrix);
-		printf("size of Matrix: %p\n", sizeof(Matrix));
+		printf("controlEffect: %f,%f,%f\n\n",controlEffect->x,controlEffect->y,controlEffect->z);
+		
+		printf("countMatrix: %d\n", countMatrix);
+		printf("size of Matrix: %d\n", sizeof(Matrix));
+		printf("size of char: %d\n", sizeof(char));
+		printf("size of int: %d\n", sizeof(int));
+		printf("size of float: %d\n", sizeof(float));
+		printf("size of double: %d\n", sizeof(double));
+		printf("size of long: %d\n", sizeof(long));
+		printf("size of long double: %d\n", sizeof(long double));
+
 		printf("address of Kp: %p\n", Kp);
 		printf("address of Kd: %p\n", Kd);
-		printf("address of Kd1: %p\n", Kd1);
-		printf("address of Kd2: %p\n", Kd2);
-		printf("address of Kd3: %p\n", Kd3);
-		printf("address of Kd4: %p\n", Kd4);
-		printf("address of Kd5: %p\n", Kd5);
-		printf("address of Kd6: %p\n", Kd6);
-		/*printf("address of Kd7: %p\n", Kd7);
-		printf("address of Kd8: %p\n", Kd8);
-		printf("address of Kd9: %p\n", Kd9);
-		printf("address of Kd10: %p\n", Kd10);
-		printf("address of Kd11: %p\n", Kd11);
-		printf("address of Kd12: %p\n", Kd12);*/
-		printf("size of char: %p\n", sizeof(char));
-		printf("size of int: %p\n", sizeof(int));
-		printf("size of float: %p\n", sizeof(float));
-		printf("size of double: %p\n", sizeof(double));
-		printf("size of long: %p\n", sizeof(long));
-		printf("size of long double: %p\n", sizeof(long double));
-		
 		for (int i = 0; i < countMatrix; i++)
 		{
 			printf("pointerMatrix %d: %p\n", i,pointerMatrix[i]);
--- dsPIC_oriented_C_develop_ANSI_C/dsPIC_oriented_C_develop_ANSI_C/matrix.c
+++ dsPIC_oriented_C_develop_ANSI_C/dsPIC_oriented_C_develop_ANSI_C/matrix.c
@@ -96,7 +96,7 @@ Matrix *m_minus(Matrix*m,Matrix*n)
     {
         for(j=0;j<3;j++)
         {
-            temp->triMatrix[i][j] = m->triMatrix[i][i] - n->triMatrix[i][j];
+            temp->triMatrix[i][j] = m->triMatrix[i][j] - n->triMatrix[i][j];
         }
     }
     return temp;
